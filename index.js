e00c05263a23a3b8d43982c2bd0cf2b02e7bd71f
Rameez <muhammadrameezahmed73@gmail.com>
2022-09-14 11:54:09 +0500

    Understand GET,POST http request method with the help of Login form

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express=require("express");
 const app= express();
+const path=require("path");
 const port = 3000;
+
+
+app.get("/",(req,res)=>{
+   
+    res.sendFile(path.join(__dirname + "/index.html"))
+    
+})
+
+app.post("/api/v1/login",(req,res)=>{
+    res.send("<h1>Form submit</h1>")
+    console.log("name")
+})
+
 
 
 app.listen(port,()=>{
